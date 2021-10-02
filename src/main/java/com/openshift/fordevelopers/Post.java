7083ce4fb0b71bde23220aeff4c492252fbb3819
selcuksert <selcuk.sert@gmail.com>
2021-10-02 18:31:08 +0300

    Added hostname field

--- src/main/java/com/openshift/fordevelopers/Post.java
+++ src/main/java/com/openshift/fordevelopers/Post.java
@@ -3,6 +3,8 @@ package com.openshift.fordevelopers;
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 
 import javax.persistence.Entity;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.Date;
 import java.util.Objects;
 
@@ -10,14 +12,20 @@ import java.util.Objects;
 public class Post extends PanacheEntity {
     private String title;
     private String content;
+    private String hostname;
     private long timestamp;
 
     public Post() {
     }
 
     public Post(String title, String content) {
-        this.title = title.toUpperCase();
+        this.title = title;
         this.content = content;
+        try {
+            this.hostname = InetAddress.getLocalHost().getHostName();
+        } catch (UnknownHostException e) {
+            this.hostname = "";
+        }
         this.timestamp = new Date().getTime();
     }
 
@@ -53,7 +61,7 @@ public class Post extends PanacheEntity {
         Post other = (Post) obj;
 
         return Objects.equals(this.title, other.title) && Objects.equals(this.content, other.content)
-                && Objects.equals(this.timestamp, other.timestamp);
+                && Objects.equals(this.timestamp, other.timestamp) && Objects.equals(this.hostname, other.hostname);
     }
 
     @Override
