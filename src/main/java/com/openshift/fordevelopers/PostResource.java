9ec8e47e8ab44741499a71192ce8f5f988f8bfad
selcuksert <selcuk.sert@gmail.com>
2021-10-02 19:26:19 +0300

    Added hostname field

--- src/main/java/com/openshift/fordevelopers/PostResource.java
+++ src/main/java/com/openshift/fordevelopers/PostResource.java
@@ -1,44 +1,45 @@
-
 package com.openshift.fordevelopers;
+
+import com.google.gson.Gson;
+import io.quarkus.hibernate.orm.panache.Panache;
+import org.jboss.logging.Logger;
 
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-
-import io.quarkus.hibernate.orm.panache.Panache;
 
 
 @Path("/posts")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class PostResource {
-  public PostResource() {
-  }
+    private static final Logger LOGGER = Logger.getLogger(PostResource.class);
+    private static final Gson gson = new Gson();
 
-  @GET
-  public Response list() {
-    return Response.ok(Post.listAll()).build();
-  }
+    public PostResource() {
+        // No code
+    }
 
-  @POST
-  @Transactional
-  public Response add(Post post) {
-    post.persist();
-    return Response.ok(Post.listAll()).build();
-  }
+    @GET
+    public Response list() {
+        return Response.ok(Post.listAll()).build();
+    }
 
-  @DELETE
-  @Transactional
-  public Response delete(Post post) {
-    EntityManager em = Panache.getEntityManager();
-    em.remove(em.contains(post) ? post : em.merge(post));
-    return Response.ok().build();
-  }
+    @POST
+    @Transactional
+    public Response add(Post post) {
+        LOGGER.infof("Persisting post: %s", gson.toJson(post));
+        post.persist();
+        return Response.ok(Post.listAll()).build();
+    }
+
+    @DELETE
+    @Transactional
+    public Response delete(Post post) {
+        EntityManager em = Panache.getEntityManager();
+        em.remove(em.contains(post) ? post : em.merge(post));
+        return Response.ok().build();
+    }
 }
